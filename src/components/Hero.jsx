58b52a3f19796f622097168d04d01a540e780f6c
varundhand <varundhand77@gmail.com>
2023-04-22 01:21:24 +0530

    added navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import {newLogo} from '../assets'
+// import React from 'react'
+// import {newLogo} from '../assets'
+import { ReactComponent as NewLogo } from '../assets/newLogo.svg'
+
 
 const Hero = () => {
-  console.log(newLogo)
+  // console.log(newLogo)
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className="flex justify-between items-cener flex-col">
-        {/* <img src={} alt="" /> */}
+      <nav className="flex justify-between items-cener w-full mb-10 pt-3">
+        <NewLogo />
+        <button
+          type='button'
+          onClick={() => window.open('https://github.com/varundhand')}
+          className='black_btn'
+        >Github</button>
       </nav>
     </header>
   )
