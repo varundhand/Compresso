9e9cab66b7a10f7716cd51d91286e2608e289cc6
varundhand <varundhand77@gmail.com>
2023-04-22 01:45:34 +0530

    hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
   // console.log(newLogo)
   return (
     <header className='w-full flex justify-center items-center flex-col'>
+
       <nav className="flex justify-between items-cener w-full mb-10 pt-3">
         <NewLogo />
         <button
@@ -15,6 +16,15 @@ const Hero = () => {
           className='black_btn'
         >Github</button>
       </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden'/>
+        <span className='orange_gradient'> OpenAI GPT-4 </span>
+      </h1>
+
+      <h2 className='desc'>
+      Introducing Compresso, an open-source article summarizer that offers a streamlined reading experience. With Compresso, lengthy articles are transformed into concise and clear summaries, enabling you to easily grasp the main points without having to spend hours reading through paragraphs. Compresso is designed to simplify your reading and help you stay informed, no matter how busy your schedule may be. Best of all, its completely free and open-source, so you can trust that your data is secure and your privacy is protected.
+      </h2>
     </header>
   )
 }
