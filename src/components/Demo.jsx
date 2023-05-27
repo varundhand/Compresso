038fbf706a4886261e5b746878e6776e605d3bf0
varundhand <varundhand77@gmail.com>
2023-05-27 22:01:06 +0530

    rendering url history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,28 +1,40 @@
 // import { useState, useEffect } from "react"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {ReactComponent as LinkSVG} from '../assets/link.svg'
+import { copy, linkIcon, loader, tick } from "../assets";
 
 import { useLazyGetSummaryQuery } from '../services/article'
 
 const Demo = () => {
   // const [{url,summary},setArticle] = useState({  //destructered them on the spot
-    const [article,setArticle] = useState({
-    url: '',
-    summary: ''
+  const [article,setArticle] = useState({ // current article
+  url: '',
+  summary: ''
   })
+  const [allArticles, setAllArticles] = useState([])
   
-  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery()
+  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles')); // retrieving all articles from local storage
+    if (articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     const {data} = await getSummary({articleUrl: article.url});
 
     if (data?.summary){ // if data object exists with summary property
       const newArticle = {...article, summary: data.summary};
+      const updatedAllArticles = [newArticle,...allArticles ]
       
       setArticle(newArticle)
-      console.log(newArticle)
+      setAllArticles(updatedAllArticles)
+
+      localStorage.setItem('articles',JSON.stringify(updatedAllArticles)) // setting all articles in local storage
     }
   }
 
@@ -55,7 +67,28 @@ const Demo = () => {
               ↩
             </button>
           </form>
-          
+
+          {/* URL HISTORY  */}
+          <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+            {allArticles.map((item,index) => (
+              <div
+                key={`link-${index}`}
+                onClick={() => setArticle(item)}
+                className="link_card"
+              >
+                <div className="copy_btn">
+                  <img 
+                    src={copy} 
+                    alt='copy_icon' 
+                    className='w-[40%] h-[40%] object-contain'
+                  />
+                </div>
+              <p className='flex-1 font-satoshi font-medium text-sm truncate text-emerald-500'>
+                {item.url}
+              </p>
+              </div>
+            ))}  
+          </div>
         </div>
       </section>
     </div>
